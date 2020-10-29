2a43756afc7340449bda6bcbbf52ef095165b4e7
VianneyDiris <vianney.diris@gmail.com>
2020-10-29 10:48:33 +0100

    add basic structure

---
+++ src/main/java/com/vianneydiris/kinomap/KinomapApplication.java
@@ -0,0 +1,13 @@
+package com.vianneydiris.kinomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinomapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinomapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vianneydiris/kinomap/KinomapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vianneydiris.kinomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinomapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
